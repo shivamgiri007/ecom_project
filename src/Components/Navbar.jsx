89e1cfdd14e7f241fb468d986c45109840e68f13
shivamgiri007 <shivamgiri.ng@gmail.com>
2023-08-13 12:45:05 +0530

    list items added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "../images/80601325.jpg";  
-import "./Navbar.css"
+import "./Navbar.css";
+import HomeList from "./NavbarListItems/HomeList";
 
 
 export default function Navbar() {
@@ -16,28 +17,28 @@ export default function Navbar() {
       <img src={logo} alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
     </a>
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="./">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="./">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="./">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="./">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="./">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="./">Home</a>
-        </li>
+      <li className="nav-item dropdown">
+          <HomeList />
+      </li>
+      <li className="nav-item dropdown">
+          <HomeList />
+      </li>
+      <li className="nav-item dropdown">
+          <HomeList />
+      </li>
+      <li className="nav-item dropdown">
+          <HomeList />
+      </li>
+      <li className="nav-item dropdown">
+          <HomeList />
+      </li>
+      <li className="nav-item dropdown">
+          <HomeList />
+      </li>
       </ul>
       <form className="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
       </form>
     </div>
   
