9bf9e86770983aa4a32f58c66d231dc708c94b9e
shivamgiri007 <shivamgiri.ng@gmail.com>
2023-08-13 11:12:57 +0530

    myntra logo set

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,61 +1,47 @@
 import React from "react";
-import logo from "../images/80601325.jpg";
+import logo from "../images/80601325.jpg";  
+import "./Navbar.css"
+
 
 export default function Navbar() {
   return (
     <>
-      <nav className="navbar">
-        <div className="myntra_logo">
-          <img src={logo} alt="myntra logo" />
-        </div>
-        <div className="list_items">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="./">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="./">
-                WOMEN
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="./">
-                KIDS
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="./">
-                HOME & LIVING
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="./">
-                BEAUTY
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="./">
-                STUDIO
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="searchbar">
-          <form class="d-flex" role="search">
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-          </form>
-        </div>
-        <div className="profile">
-        <i class="fa-regular fa-user"></i>
-     </div>
-      </nav>
+      <nav className="navbar navbar-expand-lg">
+  {/* < className="container-fluid"> */}
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarText">
+    <a className="navbar-brand myntra-logo" href="./">
+      <img src={logo} alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
+    </a>
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="./">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="./">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="./">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="./">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="./">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="./">Home</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  
+</nav>
     </>
   );
 }
